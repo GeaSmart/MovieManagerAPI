b468f5e68ddffb9ed9cfd998c230646aa6f7af28
Gerson Azabache <gerson_dunk@hotmail.com>
2021-12-22 17:07:16 -0500

    first entity added, basic configuration done

---
+++ MovieManagerAPI/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieManagerAPI.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieManagerAPI
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Genero> Generos { get; set; }
+    }
+}
--- MovieManagerAPI/Startup.cs
+++ MovieManagerAPI/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +26,9 @@ namespace MovieManagerAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDBContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("defaultConnection"))
+            );
 
             services.AddControllers();
         }
