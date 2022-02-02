70279577f14593657cc5288ef0a61a80f5b2a337
Gerson Azabache <gerson_dunk@hotmail.com>
2022-02-02 16:12:39 -0500

    creating custom base controller class

---
+++ MovieManagerAPI/Controllers/CustomBaseController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieManagerAPI.Controllers
+{
+    public class CustomBaseController: ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+        private readonly IMapper mapper;
+
+        public CustomBaseController(ApplicationDBContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+
+    }
+}
