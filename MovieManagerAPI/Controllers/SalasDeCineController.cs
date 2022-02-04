d510af09cd9c7008d05764f8675c347a828bbea6
Gerson Azabache <gerson_dunk@hotmail.com>
2022-02-04 16:49:51 -0500

    topology completed

--- MovieManagerAPI/Controllers/SalasDeCineController.cs
+++ MovieManagerAPI/Controllers/SalasDeCineController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MovieManagerAPI.DTO;
 using MovieManagerAPI.Entidades;
+using NetTopologySuite.Geometries;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,11 +17,13 @@ namespace MovieManagerAPI.Controllers
     {
         private readonly ApplicationDBContext context;
         private readonly IMapper mapper;
+        private readonly GeometryFactory geometryFactory;
 
-        public SalasDeCineController(ApplicationDBContext context, IMapper mapper) : base(context, mapper)
+        public SalasDeCineController(ApplicationDBContext context, IMapper mapper, GeometryFactory geometryFactory) : base(context, mapper)
         {
             this.context = context;
             this.mapper = mapper;
+            this.geometryFactory = geometryFactory;
         }
 
         [HttpGet]
@@ -33,6 +37,25 @@ namespace MovieManagerAPI.Controllers
         {
             return await Get<SalaDeCine, SalaDeCineDTO>(id);
         }
+
+        [HttpGet("Cercanos")]
+        public async Task<ActionResult<List<SalaDeCineCercanoDTO>>> Cercanos([FromQuery] SalaDeCineCercanoFiltroDTO filtro)
+        {
+            var puntoReferencia = geometryFactory.CreatePoint(new Coordinate(filtro.Longitud, filtro.Latitud));
+            var salasDeCine = await context.SalasDeCine
+                .OrderBy(x => x.Ubicacion.Distance(puntoReferencia))
+                .Where(x => x.Ubicacion.IsWithinDistance(puntoReferencia, filtro.DistanciaEnKms * 1000))
+                .Select(x => new SalaDeCineCercanoDTO
+                {
+                    Id = x.Id,
+                    Nombre = x.Nombre,
+                    Longitud = x.Ubicacion.X,
+                    Latitud = x.Ubicacion.Y,
+                    DistanciaEnMetros = Math.Round(x.Ubicacion.Distance(puntoReferencia))
+                }).ToListAsync();
+            return salasDeCine;
+        }
+            
 
         [HttpPost]
         public async Task<ActionResult> Post([FromBody] SalaDeCineCreacionDTO salaDeCineCreacionDTO)
---
+++ MovieManagerAPI/DTO/SalaDeCineCercanoFiltroDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieManagerAPI.DTO
+{
+    public class SalaDeCineCercanoFiltroDTO
+    {
+        [Range(-90, 90)]
+        public double Latitud { get; set; }
+        [Range(-180, 180)]
+        public double Longitud { get; set; }
+
+        private int distanciaEnKms = 10; //distancia por defecto
+        private int distanciaEnKmsMaxima = 500;
+        public int DistanciaEnKms {
+            get{
+                return distanciaEnKms;
+            }
+            set{
+                distanciaEnKms = (value > distanciaEnKmsMaxima) ? distanciaEnKmsMaxima : value;
+            } 
+        }
+    }
+}
