b1f2804d02c36ea8ffd125c37a2ae3455f35b113
Gerson Azabache <gerson_dunk@hotmail.com>
2022-02-07 11:23:01 -0500

    added library for identity and updated applicationdbcontext

--- MovieManagerAPI/ApplicationDBContext.cs
+++ MovieManagerAPI/ApplicationDBContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MovieManagerAPI.Entidades;
 using System;
 using System.Collections.Generic;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace MovieManagerAPI
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext
     {
         public ApplicationDBContext(DbContextOptions options) : base(options)
         {
