8eb03900f6236ac6f6278e2f3d5466014d909a24
Gerson Azabache Martinez <gerson_dunk@hotmail.com>
2022-02-10 03:00:05 -0500

    first test method, check it

---
+++ MovieManagerAPI.Tests/PruebasUnitarias/GenerosControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieManagerAPI.Controllers;
+using MovieManagerAPI.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieManagerAPI.Tests.PruebasUnitarias
+{
+    [TestClass]
+    public class GenerosControllerTests : BasePruebas
+    {
+        [TestMethod]
+        public async Task ObtenerTodosLosGeneros()
+        {
+            //Preparacion
+            var nombreBD = Guid.NewGuid().ToString();
+            var contexto = ConstruirContext(nombreBD);
+            var mapper = ConfigurarAutomapper();
+
+            contexto.Generos.Add(new Genero { Nombre = "género 1" });
+            contexto.Generos.Add(new Genero { Nombre = "género 2" });
+            await contexto.SaveChangesAsync();
+
+            var contexto2 = ConstruirContext(nombreBD);
+
+            //Prueba
+            var controlador = new GenerosController(contexto2, mapper);
+            var respuesta = await controlador.Get();
+
+            //Verificación
+            var generos = respuesta;
+            Assert.AreEqual(2, generos.Count);
+
+        }
+    }
+}
