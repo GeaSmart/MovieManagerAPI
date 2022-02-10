55366abbf1ceed1bd398ff17c5b1a900cf577cc8
Gerson Azabache Martinez <gerson_dunk@hotmail.com>
2022-02-09 23:25:46 -0500

    unit testing configured and basepruebas class done

---
+++ MovieManagerAPI.Tests/BasePruebas.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using MovieManagerAPI.Helpers;
+using NetTopologySuite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieManagerAPI.Tests
+{
+    public class BasePruebas
+    {
+        protected ApplicationDBContext ConstruirContext(string nombreDb)
+        {
+            var opciones = new DbContextOptionsBuilder<ApplicationDBContext>().UseInMemoryDatabase(nombreDb).Options;
+            var dbContext = new ApplicationDBContext(opciones);
+            return dbContext;
+        }
+
+        protected IMapper ConfigurarAutomapper()
+        {
+            var config = new MapperConfiguration(options => {
+                var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+                options.AddProfile(new AutoMapperProfiles(geometryFactory));
+            });
+
+            return config.CreateMapper();
+        }
+    }
+}
