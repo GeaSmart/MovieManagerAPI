4803f52677c83de9d6b7067882adb476488d74c9
Gerson Azabache Martinez <gerson_dunk@hotmail.com>
2022-02-10 23:49:59 -0500

    third validation in the first method for actores controller

--- MovieManagerAPI.Tests/PruebasUnitarias/ActoresControllerTests.cs
+++ MovieManagerAPI.Tests/PruebasUnitarias/ActoresControllerTests.cs
@@ -29,7 +29,7 @@ namespace MovieManagerAPI.Tests.PruebasUnitarias
 
             var contexto2 = ConstruirContext(nombreBD);
 
-            //Prueba
+            //Prueba 1
             var controlador = new ActoresController(contexto2, mapper, null);
 
             //porque para usar la paginación se hace uso de InsertarParametrosPaginacion el cual necesita un httpcontext, si no agregamos esta línea tendremos un nullreference exception
@@ -37,18 +37,27 @@ namespace MovieManagerAPI.Tests.PruebasUnitarias
             var paginacionDTO = new PaginacionDTO() { CantidadRegistrosPorPagina = 10, Pagina = 1 };
             var respuesta = await controlador.Get(paginacionDTO);
 
-            //Verificación
+            //Verificación 1
             var actores = respuesta.Value;
             Assert.AreEqual(3, actores.Count);
 
-
+            //prueba 2
             controlador.ControllerContext.HttpContext = new DefaultHttpContext();
             var paginacionDTO2 = new PaginacionDTO() { CantidadRegistrosPorPagina = 2, Pagina = 2 };
             var respuesta2 = await controlador.Get(paginacionDTO2);
 
-            //Verificación
+            //Verificación 2
             var actores2 = respuesta2.Value;
             Assert.AreEqual(1, actores2.Count);
+
+            //prueba 3
+            controlador.ControllerContext.HttpContext = new DefaultHttpContext();
+            var paginacionDTO3 = new PaginacionDTO() { CantidadRegistrosPorPagina = 2, Pagina = 3 };
+            var respuesta3 = await controlador.Get(paginacionDTO3);
+
+            //Verificación 3
+            var actores3 = respuesta3.Value;
+            Assert.AreEqual(0, actores3.Count);
         }
 
         
