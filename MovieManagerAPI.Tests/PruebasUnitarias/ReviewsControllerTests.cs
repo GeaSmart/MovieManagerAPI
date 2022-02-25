84830a54a9f5212ede9dd94b2adc8fda80d8dbe5
Gerson Azabache <gerson_dunk@hotmail.com>
2022-02-25 17:59:27 -0500

    reviewscontrollertests completed

--- MovieManagerAPI.Tests/PruebasUnitarias/ReviewsControllerTests.cs
+++ MovieManagerAPI.Tests/PruebasUnitarias/ReviewsControllerTests.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieManagerAPI.Controllers;
+using MovieManagerAPI.DTO;
+using MovieManagerAPI.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +13,77 @@ using System.Threading.Tasks;
 namespace MovieManagerAPI.Tests.PruebasUnitarias
 {
     [TestClass]
-    public class ReviewsControllerTests
+    public class ReviewsControllerTests : BasePruebas
     {
+        [TestMethod]
+        public async Task UsuarioNoPuedeCrearDosReviewsParaMismaPelicula()
+        {
+            var nombreBD = Guid.NewGuid().ToString();
+            var contexto = ConstruirContext(nombreBD);
+            SeedData(nombreBD);
 
+            var peliculaId = contexto.Peliculas.Select(x => x.Id).First();
+            var review1 = new Review()
+            {
+                PeliculaId = peliculaId,
+                UsuarioId = usuarioPorDefectoId,
+                Puntuacion = 5
+            };
+
+            contexto.Add(review1);
+            await contexto.SaveChangesAsync();
+
+            var contexto2 = ConstruirContext(nombreBD);
+            var mapper = ConfigurarAutomapper();
+
+            var controller = new ReviewsController(contexto2, mapper);
+            controller.ControllerContext = ConstruirControllerContext();
+
+            var reviewCreacionDTO = new ReviewCreacionDTO()
+            {
+                Puntuacion = 5
+            };
+            var respuesta = await controller.Post(peliculaId, reviewCreacionDTO);
+
+            var valor = respuesta as IStatusCodeActionResult;
+            Assert.AreEqual(400, valor.StatusCode.Value);
+        }
+
+        [TestMethod]
+        public async Task CrearReview()
+        {
+            var nombreBD = Guid.NewGuid().ToString();
+            var contexto = ConstruirContext(nombreBD);
+            SeedData(nombreBD);
+
+            var peliculaId = contexto.Peliculas.Select(x => x.Id).First();
+            var contexto2 = ConstruirContext(nombreBD);
+
+            var mapper = ConfigurarAutomapper();
+            var controller = new ReviewsController(contexto2, mapper);
+            controller.ControllerContext = ConstruirControllerContext();
+
+            var reviewCreacionDTO = new ReviewCreacionDTO()
+            {
+                Puntuacion = 5
+            };
+            var respuesta = await controller.Post(peliculaId, reviewCreacionDTO);
+
+            var valor = respuesta as NoContentResult;
+
+            Assert.IsNotNull(valor);
+            var contexto3 = ConstruirContext(nombreBD);
+            var reviewBD = contexto3.Reviews.First();
+            Assert.IsNotNull(usuarioPorDefectoId, reviewBD.UsuarioId);
+
+        }
+
+        //Método auxiliar
+        private void SeedData(string nombreBD)
+        {
+            var contexto = ConstruirContext(nombreBD);
+            contexto.Peliculas.Add(new Pelicula { Titulo = "Tiburon de prueba" });
+            contexto.SaveChanges();
+        }        
     }
 }
