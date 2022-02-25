2b6db11774e670fd34cd4d99a500e62f875ea683
Gerson Azabache <gerson_dunk@hotmail.com>
2022-02-25 17:45:08 -0500

    adding context features into basepruebas

--- MovieManagerAPI.Tests/BasePruebas.cs
+++ MovieManagerAPI.Tests/BasePruebas.cs
@@ -1,10 +1,13 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MovieManagerAPI.Helpers;
 using NetTopologySuite;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +15,9 @@ namespace MovieManagerAPI.Tests
 {
     public class BasePruebas
     {
+        protected string usuarioPorDefectoId = "940533e0-e9fe-442f-9184-8a27d7b2911b";
+        protected string usuarioPorDefectoEmail = "ejemplo@ejemplo.com";
+
         protected ApplicationDBContext ConstruirContext(string nombreDb)
         {
             var opciones = new DbContextOptionsBuilder<ApplicationDBContext>().UseInMemoryDatabase(nombreDb).Options;
@@ -27,6 +33,17 @@ namespace MovieManagerAPI.Tests
             });
 
             return config.CreateMapper();
+        }
+
+        protected ControllerContext ConstruirControllerContext()
+        {
+            var usuario = new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {
+                new Claim(ClaimTypes.Name, usuarioPorDefectoEmail),
+                new Claim(ClaimTypes.Email, usuarioPorDefectoEmail),
+                new Claim(ClaimTypes.NameIdentifier,usuarioPorDefectoId)
+            }));
+
+            return new ControllerContext() { HttpContext = new DefaultHttpContext() { User = usuario } };
         }
     }
 }
---
+++ MovieManagerAPI.Tests/PruebasUnitarias/ReviewsControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieManagerAPI.Tests.PruebasUnitarias
+{
+    [TestClass]
+    public class ReviewsControllerTests
+    {
+
+    }
+}
